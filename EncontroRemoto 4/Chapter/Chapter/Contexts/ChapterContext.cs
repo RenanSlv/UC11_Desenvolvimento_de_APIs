e5d1f5eb7db2b12fdd37e23f7a1f156645c1a827
RenanSlv <renan.romulo1337@gmail.com>
2023-03-06 11:53:55 -0300

    conteudo desenvolvido no ER 4

---
+++ EncontroRemoto 4/Chapter/Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,24 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Chapter.Contexts
+{
+    //Comunicação entre API e o Banco de dados
+    public class ChapterContext : DbContext
+    {
+        // Metodo construtor vazio
+        public ChapterContext() { }
+        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source = DESKTOP-NJHHBJS\\SQLEXPRESS; initial catalog = Chapter;Integrated Security = true"); 
+            }
+
+        }
+        public DbSet<Livro> Livros { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
